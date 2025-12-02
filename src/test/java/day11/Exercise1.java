218823d61c454b0b1e53ded09e40c76bd04f0240
cuongnguyen3108 <nguyencaocuong31082001@gmail.com>
2025-12-02 21:21:29 +0700

    add Exercise 1

---
+++ src/test/java/day11/Exercise1.java
@@ -0,0 +1,15 @@
+package day11;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Exercise1 {
+    static WebDriver driver = null;
+
+    public static void main(String[] args) {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://serenity-bdd.github.io/");
+        driver.quit();
+    }
+}
