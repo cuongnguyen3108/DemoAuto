4f6c535fb01374688262a29b4d5f11ab4a7bf73e
cuongnguyen3108 <nguyencaocuong31082001@gmail.com>
2025-12-19 13:59:51 +0700

    Add conditions to addProductToCart function

--- src/test/java/pages/InventoryPage.java
+++ src/test/java/pages/InventoryPage.java
@@ -50,8 +50,8 @@ public class InventoryPage extends BasePage {
             products.add(product);
         }
         WebElement totalShoppingCart = WaitElement.visible(driver, By.className("shopping_cart_badge"), 10);
-            if (Integer.parseInt(totalShoppingCart.getText())!=q){
-            System.out.println("The total number of products in the cart is incorrect!");
+        if (Integer.parseInt(totalShoppingCart.getText()) != q) {
+            System.out.println("The total number ofFproducts in the cart is incorrect!");
         } else {
             System.out.println("The total number of products in the cart:: " + totalShoppingCart.getText());
             totalShoppingCart.click();
