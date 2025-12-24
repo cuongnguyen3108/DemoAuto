da9be7755da1842c74fec46fefd967a54f271efa
cuongnguyen3108 <nguyencaocuong31082001@gmail.com>
2025-12-24 13:13:06 +0700

    update read file excel in LoginPage.java

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import vn.devpro.assignment67.utils.ElementValidate;
 import vn.devpro.assignment67.utils.ExelUtils;
 import vn.devpro.assignment67.utils.WaitElement;
+import vn.devpro.assignment67.utils.helpers.LocatorHelper;
 
 import java.util.Map;
 
@@ -37,15 +38,15 @@ public class LoginPage extends BasePage {
         System.out.println("\n\nLogin");
         Map<String, String> firstRow = excelData.get(0);
         for (Map<String, String> data : excelData) {
-            WebElement inputUsername = WaitElement.visible(driver,By.xpath(firstRow.get("element_username")), 10);
+            WebElement inputUsername = WaitElement.visible(driver, LocatorHelper.getBy(firstRow.get("element_username")), 10);
             ElementValidate.clearAndType(inputUsername,data.get(username));
-            WebElement inputPassword = WaitElement.visible(driver, By.xpath(firstRow.get("element_password")), 10);
+            WebElement inputPassword = WaitElement.visible(driver,LocatorHelper.getBy(firstRow.get("element_password")), 10);
             ElementValidate.clearAndType(inputPassword,data.get(password));
 
-            WebElement btnLogin = WaitElement.visible(driver, By.xpath(firstRow.get("element_btnLogin")), 10);
+            WebElement btnLogin = WaitElement.visible(driver,LocatorHelper.getBy(firstRow.get("element_btnLogin")), 10);
             btnLogin.click();
 
-            String msg = ElementValidate.validate(driver, null, "Login", By.cssSelector(firstRow.get("element_msg")));
+            String msg = ElementValidate.validate(driver, null, "Login", LocatorHelper.getBy(firstRow.get("element_msg")));
             System.out.println(msg);
         }
 
