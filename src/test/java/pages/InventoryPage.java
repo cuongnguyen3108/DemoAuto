69c09214aea0e6c24c5cc2ccbd95719686bda608
cuongnguyen3108 <nguyencaocuong31082001@gmail.com>
2025-12-24 13:24:58 +0700

    update read file excel in InventoryPage.java

--- src/test/java/pages/InventoryPage.java
+++ src/test/java/pages/InventoryPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import vn.devpro.assignment67.models.Product;
 import vn.devpro.assignment67.utils.WaitElement;
+import vn.devpro.assignment67.utils.helpers.LocatorHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,11 +38,11 @@ public class InventoryPage extends BasePage {
         System.out.println("\n\nPage Add To Cart");
         Map<String, String> firstRow = excelData.get(0);
 
-        WebElement sortProduct = WaitElement.clickable(driver, By.xpath(firstRow.get("element_selectSort")), 5);
+        WebElement sortProduct = WaitElement.clickable(driver, LocatorHelper.getBy(firstRow.get("element_selectSort")), 5);
         Select selectSort = new Select(sortProduct);
         selectSort.selectByValue(firstRow.get("selectSort"));
 
-        List<WebElement> listProduct = WaitElement.visibleElements(driver, By.xpath(firstRow.get("element_listProduct")), 10);
+        List<WebElement> listProduct = WaitElement.visibleElements(driver, LocatorHelper.getBy(firstRow.get("element_listProduct")), 10);
         List<Product> products = new ArrayList<>();
         int q = 0;
         for (Map<String, String> data : excelData) {
@@ -65,12 +66,12 @@ public class InventoryPage extends BasePage {
             q = Math.min(listProduct.size(),requestedQty);
             for (int i = 0; i < q; i++) {
                 Product product = new Product();
-                WebElement addToCartProduct = listProduct.get(i).findElement(By.xpath(firstRow.get("element_addToCartProduct")));
+                WebElement addToCartProduct = listProduct.get(i).findElement(LocatorHelper.getBy(firstRow.get("element_addToCartProduct")));
                 addToCartProduct.click();
-                WebElement nameProduct = listProduct.get(i).findElement(By.xpath(firstRow.get("element_nameProduct")));
+                WebElement nameProduct = listProduct.get(i).findElement(LocatorHelper.getBy(firstRow.get("element_nameProduct")));
                 System.out.println("Name Product: " + nameProduct.getText());
                 product.setProductName(nameProduct.getText());
-                WebElement priceProduct = listProduct.get(i).findElement(By.xpath(firstRow.get("element_priceProduct")));
+                WebElement priceProduct = listProduct.get(i).findElement(LocatorHelper.getBy(firstRow.get("element_priceProduct")));
                 System.out.println("Price Product: " + priceProduct.getText());
                 String priceNumber = priceProduct.getText().replace("$", "");
                 product.setPrice(Double.parseDouble(priceNumber));
@@ -83,7 +84,7 @@ public class InventoryPage extends BasePage {
             }
         }
 
-        WebElement totalShoppingCart = WaitElement.visible(driver, By.className(firstRow.get("element_totalShoppingCart")), 10);
+        WebElement totalShoppingCart = WaitElement.visible(driver, LocatorHelper.getBy(firstRow.get("element_totalShoppingCart")), 10);
 
         if (Integer.parseInt(totalShoppingCart.getText()) != q) {
             System.out.println("❌ The total number ofF products in the cart is incorrect!");
