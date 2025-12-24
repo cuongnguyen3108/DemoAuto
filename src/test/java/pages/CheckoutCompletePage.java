d056a414e898edae635f033f69b383081e573e51
cuongnguyen3108 <nguyencaocuong31082001@gmail.com>
2025-12-24 16:04:41 +0700

    update read file excel in CheckoutCompletePage.java

--- src/test/java/pages/CheckoutCompletePage.java
+++ src/test/java/pages/CheckoutCompletePage.java
@@ -1,14 +1,27 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import vn.devpro.assignment67.utils.WaitElement;
+import vn.devpro.assignment67.utils.helpers.LocatorHelper;
+
+import java.util.Map;
 
 public class CheckoutCompletePage extends BasePage {
 
     public CheckoutCompletePage(WebDriver driver) {
         super(driver);
+        loadExcelData();
+    }
+
+    @Override
+    protected String getFilePath() {
+        return "data-test.xltx";
+    }
+
+    @Override
+    protected String getSheetName() {
+        return "Complete";
     }
 
     @Override
@@ -18,12 +31,13 @@ public class CheckoutCompletePage extends BasePage {
 
     public void complete() {
         System.out.println("\n\nPage Checkout: Complete!");
+        Map<String, String> firstRow = excelData.get(0);
 
-        WebElement completeHeader = WaitElement.visible(driver, By.xpath("//h2[@class=\"complete-header\"]"), 10);
+        WebElement completeHeader = WaitElement.visible(driver, LocatorHelper.getBy(firstRow.get("element_completeHeader")), 10);
         System.out.println(completeHeader.getText());
-        WebElement completeText = WaitElement.visible(driver, By.xpath("//div[@class=\"complete-text\"]"), 10);
+        WebElement completeText = WaitElement.visible(driver, LocatorHelper.getBy(firstRow.get("element_completeText")), 10);
         System.out.println(completeText.getText());
-        WebElement btnBackHome = WaitElement.clickable(driver, By.id("back-to-products"), 15);
+        WebElement btnBackHome = WaitElement.clickable(driver, LocatorHelper.getBy(firstRow.get("element_btnBackHome")), 15);
         if (!btnBackHome.isDisplayed()) {
             System.out.println("❌ Button " + btnBackHome.getText() + " is not displayed");
             driver.quit();
