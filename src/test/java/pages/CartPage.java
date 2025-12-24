5482d9d567b7e1c6c9207095b72ea12acf568cfc
cuongnguyen3108 <nguyencaocuong31082001@gmail.com>
2025-12-24 16:16:18 +0700

    update read file excel in CartPage.java

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -1,17 +1,30 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import vn.devpro.assignment67.models.Product;
 import vn.devpro.assignment67.utils.WaitElement;
+import vn.devpro.assignment67.utils.helpers.LocatorHelper;
 
 import java.util.List;
+import java.util.Map;
 
 public class CartPage extends BasePage {
 
     public CartPage(WebDriver driver) {
         super(driver);
+        loadExcelData();
+    }
+
+    @Override
+    protected String getFilePath() {
+        return "data-test.xltx";
+    }
+
+
+    @Override
+    protected String getSheetName() {
+        return "Cart";
     }
 
     @Override
@@ -21,17 +34,18 @@ public class CartPage extends BasePage {
 
     public void yourCart(List<Product> products) {
         System.out.println("\n\nPage Your Cart");
+        Map<String, String> firstRow = excelData.get(0);
 
-        WebElement totalShoppingCart = WaitElement.visible(driver, By.className("shopping_cart_badge"), 10);
+        WebElement totalShoppingCart = WaitElement.visible(driver, LocatorHelper.getBy(firstRow.get("element_totalShoppingCart")), 10);
 
         if (Integer.parseInt(totalShoppingCart.getText()) != products.size()) {
             System.out.println("❌ The total number ofF products in the cart is incorrect!");
         } else {
-            System.out.println("✅ The total number of products in the cart:: " + totalShoppingCart.getText());
+            System.out.println("✅ The total number of products in the cart: " + totalShoppingCart.getText());
             totalShoppingCart.click();
         }
 
-        List<WebElement> listProduct = WaitElement.visibleElements(driver, By.xpath("//div[@class='cart_item']"), 10);
+        List<WebElement> listProduct = WaitElement.visibleElements(driver,LocatorHelper.getBy(firstRow.get("element_listProduct")), 10);
         if (listProduct.size() != products.size()) {
             System.out.println("\tThe number of products displayed is incorrect; \n\t" + products.size() + " products were added, but " + listProduct.size() + " are shown.");
             driver.quit();
@@ -40,7 +54,7 @@ public class CartPage extends BasePage {
         System.out.println("✅ Number of products on display: " + listProduct.size());
 
         for (int i = 0; i < products.size(); i++) {
-            WebElement nameProduct = listProduct.get(i).findElement(By.xpath("./descendant::div[@class=\"inventory_item_name\"]"));
+            WebElement nameProduct = listProduct.get(i).findElement(LocatorHelper.getBy(firstRow.get("element_nameProduct")));
             if (!products.get(i).getProductName().equals(nameProduct.getText())) {
                 System.out.println("\tProduct name number " + i + 1 + " is displayed incorrectly.\n\tAdded name:" + products.get(i).getProductName() + ".\n\tDisplayed name:" + nameProduct.getText() + ".");
                 driver.quit();
@@ -48,7 +62,7 @@ public class CartPage extends BasePage {
             }
             System.out.println("Name Product " + (i + 1) + ": " + nameProduct.getText());
 
-            WebElement priceProduct = listProduct.get(i).findElement(By.xpath("./descendant::div[@class=\"inventory_item_price\"]"));
+            WebElement priceProduct = listProduct.get(i).findElement(LocatorHelper.getBy(firstRow.get("element_priceProduct")));
             if (!priceProduct.getText().equals("$" + products.get(i).getPrice())) {
                 System.out.println("\tProduct price number " + i + 1 + " is displayed incorrectly.\n\tAdded price:" + products.get(i).getPrice() + ".\n\tDisplayed price:" + priceProduct.getText() + ".");
                 driver.quit();
@@ -56,7 +70,7 @@ public class CartPage extends BasePage {
             }
             System.out.println("Price Product" + (i + 1) + ": " + priceProduct.getText());
 
-            WebElement quantityProduct = listProduct.get(i).findElement(By.xpath("./descendant::div[@class=\"cart_quantity\"]"));
+            WebElement quantityProduct = listProduct.get(i).findElement(LocatorHelper.getBy(firstRow.get("element_quantityProduct")));
             if (Integer.parseInt(quantityProduct.getText()) != products.get(i).getQuantity()) {
                 System.out.println("\tProduct quantity number " + i + 1 + " is displayed incorrectly.\n\tAdded quantity:" + products.get(i).getQuantity() + ".\n\tDisplayed quantity:" + quantityProduct.getText() + ".");
                 driver.quit();
@@ -65,14 +79,14 @@ public class CartPage extends BasePage {
             System.out.println("Quantity Product: " + quantityProduct.getText());
 
         }
-        WebElement btnContinueShopping = WaitElement.clickable(driver, By.xpath("//button[@id=\"continue-shopping\"]"), 15);
+        WebElement btnContinueShopping = WaitElement.clickable(driver, LocatorHelper.getBy(firstRow.get("element_btnContinueShopping")), 15);
         if (!btnContinueShopping.isDisplayed()) {
             System.out.println("❌ Button " + btnContinueShopping.getText() + " is not displayed");
             driver.quit();
             return;
         }
         System.out.println("✅ Button " + btnContinueShopping.getText() + " is displayed");
-        WebElement btnCheckout = WaitElement.clickable(driver, By.xpath("//button[@id=\"checkout\" and @name=\"checkout\"]"), 15);
+        WebElement btnCheckout = WaitElement.clickable(driver, LocatorHelper.getBy(firstRow.get("element_btnCheckout")), 15);
         if (!btnCheckout.isDisplayed()) {
             System.out.println("❌ Button " + btnCheckout.getText() + " is not displayed");
             driver.quit();
